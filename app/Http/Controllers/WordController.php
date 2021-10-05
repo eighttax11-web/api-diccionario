93580909dab27df32c806faf7e4294ab2803b5dd
Cristian <eighttax11@gmail.com>
2021-10-05 18:41:30 -0500

    Se termino de crear todo el CRUD de word y se añadió el método search

--- app/Http/Controllers/WordController.php
+++ app/Http/Controllers/WordController.php
@@ -17,4 +17,36 @@ class WordController extends Controller
     {
         return response()->json($this->word_repository->index());
     }
+
+    public function store(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $name = $request->input('name');
+        $url = $request->input('url');
+        $category_id = $request->input('category_id');
+        return response()->json($this->word_repository->store($name, $url, $category_id));
+    }
+
+    public function show($id): \Illuminate\Http\JsonResponse
+    {
+        return response()->json($this->word_repository->show($id));
+    }
+
+    public function update(Request $request, $id): \Illuminate\Http\JsonResponse
+    {
+        $name = $request->input('name');
+        $url = $request->input('url');
+        $category_id = $request->input('category_id');
+        return response()->json($this->word_repository->update($id, $name, $url, $category_id));
+    }
+
+    public function destroy($id): \Illuminate\Http\JsonResponse
+    {
+        return response()->json($this->word_repository->destroy($id));
+    }
+
+    public function search(Request $request): \Illuminate\Http\JsonResponse
+    {
+        $search = $request->input('search');
+        return response()->json($this->word_repository->search($search));
+    }
 }
--- app/Repositories/WordRepository.php
+++ app/Repositories/WordRepository.php
@@ -10,4 +10,62 @@ class WordRepository
     {
         return Word::with('category')->get();
     }
+
+    public function store($name, $url, $category_id)
+    {
+        $new_word['name'] = $name;
+        $new_word['url'] = $url;
+        $new_word['category_id'] = $category_id;
+
+        return Word::create($new_word);
+    }
+
+    public function find($id)
+    {
+        return Word::where('id', '=', $id)->first();
+    }
+
+    public function show($id)
+    {
+        $post = Word::find($id);
+
+        if (is_object($post)) {
+
+            return Word::find($id)->load('category');
+        }
+
+        return;
+
+    }
+
+    public function update($id, $name, $url, $category_id)
+    {
+        $word = $this->find($id);
+
+        if (empty($word)) {
+            return;
+        }
+
+        $word->name = $name;
+        $word->url = $url;
+        $word->category_id = $category_id;
+        $word->save();
+        return $word;
+    }
+
+    public function destroy($id)
+    {
+        $word = $this->find($id);
+
+        if (empty($word)) {
+            return;
+        }
+
+        return $word->delete();
+    }
+
+    public function search($search)
+    {
+        return Word::where('name', 'like', '%' . $search . '%')->get();
+    }
 }
